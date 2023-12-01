5ac942a36e02a2bf5fab13389211f224ef736b67
Huntress of the Fallen <andika.bagaskara@thatgamecompany.com>
2023-12-01 20:49:12 +0700

    fix: add both $set and $setOnInsert on CensoredWords

--- scripts/src/updateCensoredWords.js
+++ scripts/src/updateCensoredWords.js
@@ -13,12 +13,28 @@ async function updateCensoredWords(guildId, censoredWords) {
 		await modConfig.updateOne(
 			{ guildId },
 			{
+				// Update the existing element in the censoredWords array where the word matches
 				$set: {
-					'censoredWords.$[element]': censoredWord,
+					'censoredWords.$[element]': {
+						...censoredWord,
+						updatedAt: { $currentDate: true },
+					},
+				},
+				// Add the new element to the censoredWords array if the word doesn't exist
+				$setOnInsert: {
+					'censoredWords.$[element]': {
+						...censoredWord,
+						createdAt: { $currentDate: true },
+						updatedAt: { $currentDate: true },
+					},
 				},
 			},
 			{
-				arrayFilters: [{ 'element.word': censoredWord.word }],
+				// Filter the array element
+				arrayFilters: [
+					{ 'element.word': censoredWord.word },
+				],
+				// Create a new element if the word doesn't exist
 				upsert: true,
 			},
 		);
