35b55392efb4bd773da8e07ade27361944ef546f
Huntress of the Fallen <andika.bagaskara@thatgamecompany.com>
2024-01-08 12:01:37 +0700

    fix: improve the translations logging

--- scripts/translate.js
+++ scripts/translate.js
@@ -4,11 +4,12 @@ const deepl = require('deepl-node');
 module.exports = async (text, { to }) => {
 	let translator = new deepl.Translator(process.env.DEEPLAUTH_PRIMARY);
 	let usage = await translator.getUsage();
-	console.log(`DEEPL Usage (PRIMARY): ${usage.character.count}`);
+	console.log(`DEEPL Usage (PRIMARY): ${usage.character.count}, Limit: ${usage.character.limit}`);
 
 	if (usage.character.limitReached()) {
 		translator = new deepl.Translator(process.env.DEEPLAUTH_SECONDARY);
 		usage = await translator.getUsage();
+		console.log(`DEEPL Usage (SECONDARY): ${usage.character.count}, Limit: ${usage.character.limit}`);
 
 		if (usage.character.limitReached()) {
 			const response = '⚠️ Translation Pause: We\'ve hit a temporary limit.\n\nSorry for the inconvenience, but the translation feature is currently unavailable due to reaching its character limit. We\'re working to get it back up and running as soon as possible.';
--- test.js
+++ test.js
@@ -1,11 +1,54 @@
+require('dotenv').config();
 const translate = require('./scripts/translate');
+const { S_TOKEN } = process.env;
+const { Client, Options, GatewayIntentBits, Partials, Events } = require('discord.js');
 
-const test = async () => {
-	let translated = await translate('Hello, world!', { to: 'ID' });
-	console.log(translated);
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.DirectMessageReactions,
+		GatewayIntentBits.MessageContent,
+	],
+	partials: [
+		Partials.Channel,
+		Partials.Message,
+		Partials.User,
+		Partials.GuildScheduledEvent,
+		Partials.GuildMember,
+		Partials.Reaction,
+	],
+	allowedMentions: { repliedUser: false },
+	makeCache: Options.cacheEverything(),
+});
+client.login(S_TOKEN);
 
-	translated = await translate('Halo, semuanya!', { to: 'EN' });
-	console.log(translated);
-};
 
-test();
+client.on(Events.ClientReady, async () => {
+	const test = async () => {
+		// thatskygame global guild
+		const guild = await client.guilds.fetch('575762611111592007').catch(err => console.error(err.message));
+
+		// thatskygame global channel of original announcement
+		const channel = await guild.channels.fetch('1077716001493356574').catch(err => console.error(err.message));
+
+		// message id of original announcement
+		const message = await channel.messages.fetch('1184953096548397178').catch(err => console.error(err.message));
+
+		let translated = await translate(message.content, { to: 'ID' });
+		console.log(translated);
+
+		const user = await client.users.fetch('800924763320745994').catch(err => console.error(err.message));
+
+		await user.send(translated).catch(err => console.error(err.message));
+
+		translated = await translate('hayoo yg hutang puasanya blm lunas semua, segera dibayar yaa <:Popcorn:1026661093713649717>	semakin cepat semakin bagus.', { to: 'EN' });
+		console.log(translated);
+	};
+
+	await test();
+});
