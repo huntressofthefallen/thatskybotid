71a69b5309f70aa46d88573ffb57c6ee81984f76
Huntress of the Fallen <andika.bagaskara@thatgamecompany.com>
2024-01-08 11:44:25 +0700

    fix: AI Translator

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
-const axios = require('axios');
+const deepl = require('deepl-node');
 
 module.exports = async (text, { to }) => {
-	const response = await axios.post(
-		'https://api-free.deepl.com/v2/translate',
-		{
-			'text': [
-				text,
-			],
-			'target_lang': to,
-		},
-		{
-			headers: {
-				'Authorization': `DeepL-Auth-Key ${process.env.DEEPLAUTH}`,
-				'Content-Type': 'application/json',
-			},
-		},
-	);
+	let translator = new deepl.Translator(process.env.DEEPLAUTH_PRIMARY);
+	let usage = await translator.getUsage();
+
+	if (usage.character.limitReached()) {
+		translator = new deepl.Translator(process.env.DEEPLAUTH_SECONDARY);
+		usage = await translator.getUsage();
+
+		if (usage.character.limitReached()) {
+			const response = '⚠️ Translation Pause: We\'ve hit a temporary limit.\n\nSorry for the inconvenience, but the translation feature is currently unavailable due to reaching its character limit. We\'re working to get it back up and running as soon as possible.';
+			return response;
+		}
+	}
+
+	const translate = await translator.translateText(text, null, to, { formality: 'less', preserveFormatting: true });
+	const response = translate.text;
+
 	return response;
 };
